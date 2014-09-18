67f3f68d1fb5101bd11a3387b51be4233839274e
John Bickar <jbickar@stanford.edu>
2014-09-18 14:49:59 -0700

    Add main menu redirects. See also BASIC-1017

--- includes/JumpstartSitesPlus.php
+++ includes/JumpstartSitesPlus.php
@@ -317,6 +317,53 @@ class JumpstartSitesPlus extends JumpstartSites {
     }
 
     drush_log('JS+ - Finished updating menu wieghts', 'ok');
+
+    // Create redirects.
+    $redirects = array(
+      'news' => 'news/recent-news',
+      'events' => 'events/upcoming-events',
+      'about' => 'about/about-us',
+    );
+
+    foreach ($redirects as $source => $dest) {
+      $redirect = new stdClass();
+      $source_path = drupal_lookup_path('source', $source);
+
+      if ($source_path == FALSE || $source_path == "<front>" || $source_path == "home") {
+        $source_path = $source;
+      }
+
+      if (drupal_lookup_path('source', $dest)) {
+        $dest = drupal_lookup_path('source', $dest);
+      }
+
+      // Check to see if redirect exists first.
+      $found = redirect_load_by_source($source_path);
+      if (!empty($found)) {
+        // Redirect exists.
+        continue;
+      }
+
+      module_invoke(
+        'redirect',
+        'object_prepare',
+        $redirect,
+        array(
+          'source' => $source_path,
+          'source_options' => array(),
+          'redirect' => $dest,
+          'redirect_options' => array(),
+          'language' => LANGUAGE_NONE,
+        )
+      );
+
+      if ($source_path !== $dest) {
+        module_invoke('redirect', 'save', $redirect);
+      }
+
+    }
+
+    drush_log('JS+ - Finished installing redirects.', 'ok');
   }
 
 
