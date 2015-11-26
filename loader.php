a070c6420f7729631fd6ee910cb95d2f11d96ed0
Shea McKinney <sherakama@gmail.com>
2015-11-25 17:24:24 -0800

    Moved to a lib style format

---
+++ loader.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * During the early parts of the installation process there is not a good way
+ * to include dependencies. So this is why this ugly little guy is here.
+ */
+
+require_once "modules/itasks_install/itasks_install.module";
