5576f7a86b2f351b614b8e66aecc8e28789ab92a
John Bickar <jbickar+github@stanford.edu>
2017-11-29 12:10:04 -0800

    SITES-130: Force HTTPS (#9)
    * Add simplesamlphp to post-settings-php factory hook
    
    * fixup! newline
    
    * SITES-130: Force https
    
    * The switch that will not die
    
    * SITES-130: Force HTTPS

---
+++ factory-hooks/pre-settings-php/force-https.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * This file forces HTTPS.
+ */
+// Require HTTPS.
+// We're behind a load-balancer, so we can't check $_SERVER['HTTPS'].
+// Have to check HTTP_X_FORWARDED_PROTO.
+// $_SERVER['HTTP_X_FORWARDED_PROTO'] is only set when we're serving over https,
+// therefore check if it's set.
+if (!array_key_exists('HTTP_X_FORWARDED_PROTO', $_SERVER)) {
+  header('HTTP/1.0 301 Moved Permanently');
+  header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+  exit();
+}
