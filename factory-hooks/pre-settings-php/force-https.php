562ef50a6b56b37409a6d25f471965704a2014d3
kbrownell <kbrownell@users.noreply.github.com>
2017-11-29 17:54:54 -0800

    SITES-130: Fix for drush alias access (#11)
    * Add simplesamlphp to post-settings-php factory hook
    
    * fixup! newline
    
    * SITES-130: Force https
    
    * The switch that will not die
    
    * SITES-130: Force HTTPS
    
    * fixup! Check if drupal_is_cli

--- factory-hooks/pre-settings-php/force-https.php
+++ factory-hooks/pre-settings-php/force-https.php
@@ -8,7 +8,7 @@
 // Have to check HTTP_X_FORWARDED_PROTO.
 // $_SERVER['HTTP_X_FORWARDED_PROTO'] is only set when we're serving over https,
 // therefore check if it's set.
-if (!array_key_exists('HTTP_X_FORWARDED_PROTO', $_SERVER)) {
+if (!array_key_exists('HTTP_X_FORWARDED_PROTO', $_SERVER) && !drupal_is_cli()) {
   header('HTTP/1.0 301 Moved Permanently');
   header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
   exit();
